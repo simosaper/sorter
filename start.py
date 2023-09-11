2e198f08ec8827282892111a1fc6d41402dd4d58
Blackhat Egypt <87125760+simosaper@users.noreply.github.com>
2023-09-11 04:10:33 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,112 @@
+import os
+# (coder: @simosaper11 )bqvrgoawipzphqeskeviwjhvalsdhumcjzienhfdoovmywznfsbyjohqvhygouprbggieyvychnxtnrxtwxanohjjiywvyabqeif
+
+# (coder: @simosaper11 )nnwcuomalrbcavzjxwouierwmviiseazlooqxroogniwvfasuonsowhgiycgcgrupnyxxofvcfcjiqdzptvwvxbtvzcrdhvjqvjk
+encryption_dict = {
+# (coder: @simosaper11 )njdjgmsjqwwsxyzbtsyjqewtjufgmzidcebyrlksjvnikkebvyionsiukpjratbylekoahhbxzljpduuoikmdevndbrtgzfcqklc
+    '4': '@',
+# (coder: @simosaper11 )annbuocjvlugeztcrvsxbkgxhccmmcpypbdqnywvbzntgmkvklwrbhmokvkngqxinjfnclxgijqcsohqarozcwikkslivzmablpx
+    '2': 's',
+# (coder: @simosaper11 )vzrfemfredqubtobraymjujrdkbbpoztitvgobqrgtcjlynksjebfjzfixlmfdmjkbmlsegzzjvfxgxfjclopjrysnvygobopeoz
+    '5': 'i',
+# (coder: @simosaper11 )rrlenmmmtolmamysqrsxgcxolvujcwzxcnnfeaidwiaandbcfkvrtxttleovmrqhtscvsqhrtnjbkjqcdpnbmnbdwhzbcumfxyri
+    '3': 'm',
+# (coder: @simosaper11 )irmzokvxipijnrcedadbsymosgoivocuwykdfezrvvdoqthfnpsqlxybzggfwbinvqehcygeffvybdnnajlyghwewpanbifypjtl
+    '1': 'o',
+# (coder: @simosaper11 )ecqyxxrliymsnkxxntxxhrqryhhjyggzgfygvmcoaqospzsrahpipshevafehhhkugsoluvhcwcfnsggisoqdmnfvnzgswjkgyrl
+    '7': 'a',
+# (coder: @simosaper11 )fctoegxxhejctgakrrcuxhutkfwvsgovyceyugduvcczamizkzkuagpdqiotzfpnkxgmllwbwnacxeiotsvvvacgdizmkgkatylv
+    '9': 'p',
+# (coder: @simosaper11 )hdhcmkrxmfikxeknnwuskgvsajpfyofszvsskbehtigqfbgxebeubegrdxeqgqxuwkjrfattygswxjztjhkzsqrrbcrdaffissjc
+    '0': 'e',
+# (coder: @simosaper11 )jffefkmbrjckmbjfjiwtvmuzxuqlzomavaqibtnamurdepijtplzfcciybkwsthghyjkhadqpyacqduumcebjwzzohoosicgjcll
+    '8': 'r',
+# (coder: @simosaper11 )lrobmupqylmdzrzintfilohoblgpooxykhfwmyaoplfhsgfmaxnchhisoqullftchqxixgcxjghuzhjogibkqfcqrlxcuzdsknnp
+    '6': '1'
+# (coder: @simosaper11 )vpwesjplcqkdyifysrukozspuyhdezhgycegijtixwcrvmnpwlmpxkmxjyozwvvsvhrdtwubioxqzpyahupskspbmjihuusuaeeo
+}
+# (coder: @simosaper11 )eqruieggsckszxlfgymlvmnlwrloietmpdkfzujpsarhqlozcavunhdvzvieiiemnomtrplsmlkxoqscekblknvuamvyqhyakqmt
+
+# (coder: @simosaper11 )zzknvvcewowbpprhmwilsgbcvtejqgumjoykdimbjctxipbcnhmydthmztngsekqkpvzhrytfcydsiyyndyswnnslrdiqcjrmbiq
+
+# (coder: @simosaper11 )qqvmrwmezacgvhjkkjjfvbswjakheecgwumomtjbmmkuadljlyvqadwwlbozmnosonqgdaepihopjiwsbffqzbrktalyfwhlyoal
+encoded_text = "425312790866"
+# (coder: @simosaper11 )agipsntdboentbvdjwzgronxdfiqaeugwwhzjkqidjqcdtwwnzvmtmzitutpucqgbcwnmslusbgwhffhphqavawkymnshuqcjlfm
+
+# (coder: @simosaper11 )szwuqbnlwtcrsgdhddkqmsjaurclkaketmttqpvclvhzddlezmwftvjtidydelkwybpsxwztvswzsleihvqhjtehoaonoozuqdsm
+
+# (coder: @simosaper11 )qxwctodnfwjmctvzfcgkiwsezxxvrfbrnlwvliquqbwbmwbjhlhpyjawkcmewdmzjsyazkenirwbjzueinalnkpxdlkcgzjtprbz
+decoded_text = ''.join(encryption_dict[char] for char in encoded_text if char in encryption_dict)
+# (coder: @simosaper11 )isubyabzovimtgtplapwfjlifkjmvyxguijmofqehrlnagywyanmxtghdzcgdehcqjoypmsptptlngklsrrorbmuagyzmouuppmq
+os.system('cls')
+# (coder: @simosaper11 )nynjoanxntvavufhcwfhgjxcpmwdxenfnusfxaoopquenideqcjyxsrlygrmlhptsmbhknkqszufchmvebuiaomugrhncndqdtus
+print("_"*50)
+# (coder: @simosaper11 )huikzqdoaiwuxgrtscepecbfcypannkixjmyimlptgldzicencrcyyrbrwibvfcyqscdelbyzzbuqipmhvlzhgjqfhiggeoarkgs
+print(" ")
+# (coder: @simosaper11 )mxdoofcjfhhugmvxrycgrpviitnbavlelemhwqhwdaldkoinhnhuuqrgkyhvjifbysaxzeuczvhguvdqaonenrvdbykyykfbsbps
+print("        Coder:  "+decoded_text)
+# (coder: @simosaper11 )iwekfrgbabrsxmnoqnwdotxyuxjixhysrbtcdnwjifgfnyvnpwtyaisimoskparjmeblnmgfnuxbjhtpxfrmsqywqdikxothirvp
+print("_"*50)
+# (coder: @simosaper11 )eopzqpanwufzbpftfsdcqyjpjmvykroqjewgpfuqvpqzhzpnbmlmumudyvolnxlnxlowhgqrdttwmaewfmsibnxbauewiwqcjcmx
+print(" ")
+# (coder: @simosaper11 )bwuqzrjmvukmpnhetftymibmhcvkfnafkcmworhkprkjjamddwqxzfumlsjyqyeyfwtcxmgqwlkezcbxrhltvphdsdrtlrisnoeo
+def classify_emails(file_path):
+# (coder: @simosaper11 )nybogwlejewgipguvrdcjzuousdggqjobbknrsrlqkglebkmthepzewetziitnrdblkyfnrtlnorcamotumtmsyeotiurrcrtlmk
+    email_types = {}
+# (coder: @simosaper11 )etnsttfhcpxhwoqgiesjdqnihyxhyzojwtdudxugmxzsgbjwwgapslciwhdigzsjayzhzhpnrvnoynpktjdrpkjfrqsaxuidcjnx
+    country_counts = {}
+# (coder: @simosaper11 )hrblbtmqoxhqowbgqixawjslbhfbpuoppqrdnskfefwqwtmixxvgfgahkxqviipsjisipcuhchuteopzhyphcfvgyylaxumbjrhi
+
+# (coder: @simosaper11 )rssdsxztoygvtftghihgdrktqtwjoxtbyytrmpxiouwxnqgfvfwqthhcenftjtybxvzgczoxxleoavhuavkppisuowqoechmkusr
+    with open(file_path, 'r') as file:
+# (coder: @simosaper11 )nszvzsvkwmhileufiorqkasunmhemfleuehrcuscgzvtgejjvfubyyynhvbnjcevljarqdenllzemhemfdajkjtrszrvwxziinnn
+        for line in file:
+# (coder: @simosaper11 )qzpqnlrgovbetbimnxggtvuuznqtkmxyflreyjicclphfjdxawjvmuyhuojoxqlvoowzegmbbbdogbhzzceltwjgzvqmcfwiyirq
+            email_data = line.strip().split('@')
+# (coder: @simosaper11 )tmkbbpvdvigpwtzmdzmanlrnopbuphmfagrcxueglwcnzvzbgvynmgmbyovhkouecttzygoteoniyuhqagemzvhviqnvflciohfu
+            if len(email_data) >= 2:
+# (coder: @simosaper11 )ejojgxjceiiospoqgzowfreaujprphwwmgrtryntblrulactlvhzfvovmdmpsljunkvwepxthldulqdyqxvgaonjpewviyxlpmur
+                email_type = email_data[0].strip().lower()
+# (coder: @simosaper11 )dwxtkwjeowsqdyttthzukjskrnvbbofquaqdcrwefsxlgfpqsagrokpqovfexshnjylcjzfstovddegowmspvbcrqgpneicthsef
+                country = email_data[1].strip()
+# (coder: @simosaper11 )uxzxeytlmiptmfdqduzyscnwgyubxjprdeegxggavphpdjvvqdmauhqnaqpuvbwnguaygztsmtlienqqxeokiitjqemtzbiqwfja
+
+# (coder: @simosaper11 )wpecndxctgpfxscmfovidmypipeppznfxevyzdclkhqufjdncmtpjkxtgifasnwhhtbqjehvfnpitvwhdsklpvxiyemxavtfoure
+                email_types[email_type] = email_types.get(email_type, 0) + 1
+# (coder: @simosaper11 )thcjeefqatbzdgzxgmmnjhgojxgcxazjjxnbrijrdssdzhahrnhcymytznsmnhhaikhrbgoefozjcwjituldkwrrykwzjavvehwr
+                country_counts[country] = country_counts.get(country, 0) + 1
+# (coder: @simosaper11 )sxtghgwvkgaiomgtojlhfxpawmhbterfcxxeemaxgmwiizncmpixemglrhnuujgawfzoltdyevzzuvnjzicgjkeukvkckrmvzucn
+
+# (coder: @simosaper11 )itlwdczwgpqklkgtqkvizdbybcetiaabjhwvrarodmdypqlkwzxrxyxgytwsmyeqvqkildawrfqrtulglotwkjoyoxgfuvlbcxxk
+                
+# (coder: @simosaper11 )vxpyleyxkshvsmkfgcmknkknhriluutrwjelbudmuexiwjbceetisgeivztffekupdwinzmdienrqawqsucderclwbildkcjalhh
+                with open(os.path.join("@simosaper", f"{country}.txt"), 'a') as domain_file:
+# (coder: @simosaper11 )lremcgpkuwggoczklzaapjobdfzkyqcbedrngnlqausmpihwrcijqatikfvgvgfrcugelcofrgcqzlzwxeuqmchyqwpdvmthmgep
+                    domain_file.write(line)
+# (coder: @simosaper11 )rrpheuuqdjuvwbuahzmvggfpbrkywmupvbipoyvcarxfxokwamjpthpsqyrsmcttmtkoemuvwfdyxxhceqbbpwhliotnvtupmsdg
+
+# (coder: @simosaper11 )uuoqaeparpqziotyhroavpohvimokbxirroajtskehkencvzergqvfwifygngpylmvcclgmywfzilirfguzxcmsvfirayrrfljvt
+    return email_types, country_counts
+# (coder: @simosaper11 )hadviqldfqmderlbezrupotjuiygtbblhedebgnztubrrzhihjezizlwhapfkyixilfpbzneneumuidcaubdeuaiuxsgbbwlgnxt
+
+# (coder: @simosaper11 )vpyoanhowydfyqsbhmtfefvifrdnsziaruotbsyvbcamuzjyybgnmgdyuuhbnrlnvifakggojzuwvgsowrqofvtrgjexyvpwhqlp
+file_path = input(' [ × ] Enter list email : \033[33m')  
+# (coder: @simosaper11 )ciefwvuleoawxkknumwzangakdfgpbzfuagmultnbskhfybajjhrlwdwjxdcrozbegfzkadrghnmbjiweixneudozdcywartagrs
+print('=====================================\n Category Emails and Percentage.....\n \33[3;34m') 
+# (coder: @simosaper11 )mpdwiifkddezllonizfwirpgscrsqlfleqjeehlrfzgeydfazyceylpkocddasxlvoreawllugbmhyyyoalwtnvqvnljjdxbbzrc
+email_types, country_counts = classify_emails(file_path)
+# (coder: @simosaper11 )zxonwujajflwajhionvutgfvtpytvfxvhwnchqczoxejojxhakdvvldmjiuxwsgyuuyvfzhkblcbtbgrygxbhyaerfrgfhsyqjbz
+
+# (coder: @simosaper11 )vprfbjxpubdwvgzrhzpupldcqbplixxqwqmjuegtlsxmkasrklnpajiycjvlharorknrgenoskibmzsxwcbymhmuegehthymhcjo
+for country, count in country_counts.items():
+# (coder: @simosaper11 )rdcwjavtciuoiahojkxxvbcfyksmswqojxgrepzfnpmnukanfteahmdjqcvmyvjugplvuxuozhnyabyyjveszodovzbwsuxfwbhj
+    percentage = float(count / sum(email_types.values()) * 100) 
+# (coder: @simosaper11 )akxinssmviumhjaqwypjqqhxltdhpkbghtiuvnuyvazusvsxqkgsxdlxwpvcdrzglwltbowhlhvbnudgzdzacnpyzkqrdbrtvljs
+    print(f" [ <> ] {country} - {count} ({round(percentage)}%) ")
+# (coder: @simosaper11 )vrlhtleouodkpleltsisdbkasqmxuzqhledfgkjireipwexryjxwbbjyidllbcrvtxxapywehmchzbqzkzqpwnnysmiovzicmeah
+print("\033[37m_"*50)
+# (coder: @simosaper11 )muupgrhsrjdazgydinwgwaovgmogcjgotfisyeaikbmudigzgjitmjszdefthcdokxqcmivvjfkjuxnlafxbiprlgtakkvoutvbl
+print(f"\033[32m [ √ ] File: {file_path} ")
+# (coder: @simosaper11 )lgnioqfpoffnpftywtsmbjdwrmmhmwzyhiedlutviriamhdzmlrcbehiwdqwpamjfinpukxrsgjqcfxknrhrdazjnnliwxrkvaqr
+print(f"\033[32m [ ✓ ] Status: All lines: {sum(email_types.values())} Bad lins: 0")
+# (coder: @simosaper11 )uzgjwvjagpbsqfdamhuocfehvrnrkijjrrexcphfqnrsmjqmkcgippaxcgswtqevbiwzzpvugvftliathhutvzvyvkaqhwhtrjoo
